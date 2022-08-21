8d3dc67ff22d0d13585abe43fed4b9124d17ad61
HyeYeonCha <gpdus335@naver.com>
2022-08-21 16:29:14 +0900

    enum 프로퍼티와 내부 함수

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,6 +1,6 @@
 package com.example.kotlinstudy
 
-enum class PaymentStatus(val label:String) {
+enum class PaymentStatus(val label:String): Payable{
     UNPAID("미지급") {
         override fun isPayable() = true
     },
@@ -20,12 +20,27 @@ enum class PaymentStatus(val label:String) {
         }
     };
 
-    abstract fun isPayable(): Boolean
 }
 
+interface Payable {
+    fun isPayable(): Boolean
+}
 
 fun main() {
-    if (PaymentStatus.UNPAID.isPayable()) {
-        println("결제 가능함")
+//    if (PaymentStatus.UNPAID.isPayable()) {
+//        println("결제 가능함")
+//    }
+
+    // valueOf enum Class 의 상수와 일치하는 값을 찾아서 바로 인스턴스화 시켜줌
+    val paymentStatus = PaymentStatus.valueOf("PAID")
+    println(paymentStatus.label)
+
+    if (paymentStatus == PaymentStatus.PAID) {
+        println("결제 완료임")
+    }
+
+    // enum class 내부 순차 출력하기
+    for (status in PaymentStatus.values()) {
+        println("${status.name}: ${status.label} -> ${status.ordinal}")
     }
 }
