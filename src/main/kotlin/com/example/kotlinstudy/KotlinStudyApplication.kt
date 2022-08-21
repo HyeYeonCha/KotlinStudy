f8ff840366317b8a5dfd578fc95088975d440d02
HyeYeonCha <gpdus335@naver.com>
2022-08-21 16:13:01 +0900

    interface 심화 - 여러개의 interface 구현하기

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -8,7 +8,11 @@ class KotlinStudyApplication
 
 class Product(val name: String, val price: Int)
 
-interface Cart {
+// 상위 위에 또 다른 상위 interface
+interface Wheel {
+    fun roll()
+}
+interface Cart: Wheel {
     var coin: Int
     val weight : String
       get() = "200KG"
@@ -19,9 +23,24 @@ interface Cart {
             println("카드 대여")
         }
     }
+
+    override fun roll() {
+        println("카드가 데굴데굴")
+    }
+
+    fun printId() = println("123456")
+
 }
 
-class MyCart(override var coin: Int) : Cart {
+interface Order {
+    fun add(product: Product) {
+        println("${product.name} 가 주문 완료")
+    }
+
+    fun printId() = println("123123")
+}
+// interface 두 개 추가
+class MyCart(override var coin: Int) : Cart, Order {
 
     override fun add(product: Product) {
         if (coin >= 0) {
@@ -29,6 +48,14 @@ class MyCart(override var coin: Int) : Cart {
         }else {
             println("코인이 부족합니다.")
         }
+
+        // Order 의 add 호출
+        super<Order>.add(product)
+    }
+
+    override fun printId() {
+        super<Cart>.printId()
+        super<Order>.printId()
     }
 }
 
@@ -37,5 +64,8 @@ fun main(args: Array<String>) {
 //    runApplication<KotlinStudyApplication>(*args)
     val cart = MyCart(coin = 100)
     cart.rent()
+    cart.roll()
     cart.add(Product(name = "츄파춥스", price = 100))
+
+    cart.printId()
 }
