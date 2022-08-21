dd63cac9e3f54bf1dd4a82d1e4d51dbe990b4219
HyeYeonCha <gpdus335@naver.com>
2022-08-21 16:22:30 +0900

    enum class 기초

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,31 @@
 package com.example.kotlinstudy
 
-fun main() {}
+enum class PaymentStatus(val label:String) {
+    UNPAID("미지급") {
+        override fun isPayable() = true
+    },
+    PAID("지급완료") {
+        override fun isPayable(): Boolean {
+            return false
+        }
+    },
+    FAILED("지급실패") {
+        override fun isPayable(): Boolean {
+            return false
+        }
+    },
+    REFUNDED("환불") {
+        override fun isPayable(): Boolean {
+            return false
+        }
+    };
+
+    abstract fun isPayable(): Boolean
+}
+
+
+fun main() {
+    if (PaymentStatus.UNPAID.isPayable()) {
+        println("결제 가능함")
+    }
+}
