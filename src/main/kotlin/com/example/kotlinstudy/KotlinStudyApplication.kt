89f9c0dc6d4f3cc98a5d470f28cbb32e552f504c
HyeYeonCha <gpdus335@naver.com>
2022-08-21 16:02:25 +0900

    interface 공부

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -6,6 +6,36 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class KotlinStudyApplication
 
+class Product(val name: String, val price: Int)
+
+interface Cart {
+    var coin: Int
+    val weight : String
+      get() = "200KG"
+    fun add(product: Product)
+
+    fun rent() {
+        if (coin >= 0) {
+            println("카드 대여")
+        }
+    }
+}
+
+class MyCart(override var coin: Int) : Cart {
+
+    override fun add(product: Product) {
+        if (coin >= 0) {
+            println("${product.name} 이 추가되었습니다.")
+        }else {
+            println("코인이 부족합니다.")
+        }
+    }
+}
+
+
 fun main(args: Array<String>) {
-    runApplication<KotlinStudyApplication>(*args)
+//    runApplication<KotlinStudyApplication>(*args)
+    val cart = MyCart(coin = 100)
+    cart.rent()
+    cart.add(Product(name = "츄파춥스", price = 100))
 }
