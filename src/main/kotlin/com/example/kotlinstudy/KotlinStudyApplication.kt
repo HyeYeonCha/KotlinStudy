9153167ea90397394eb14b75992a5041bda5a5b8
HyeYeonCha <gpdus335@naver.com>
2022-08-29 20:19:32 +0900

    간단한 상속 예제

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,42 @@
 package com.example.kotlinstudy
 
-fun main() {}
+abstract class Developer
+{
+    abstract val name:String
+    abstract fun code(language: String)
+}
+
+data class Backend(override val name:String) : Developer() {
+    override fun code(language: String) {
+        println("backend use $language ")
+    }
+}
+
+data class Frontend(override val name:String): Developer() {
+    override fun code(language: String) {
+        println("frontend use $language")
+    }
+}
+
+object DeveloperPool {
+    val pool = mutableMapOf<String, Developer>()
+
+    fun add(developer: Developer) = when(developer) {
+        is Backend -> pool[developer.name] = developer
+        is Frontend -> pool[developer.name] = developer
+        else -> println("nope.")
+    }
+
+    fun get(name: String) = pool[name]
+}
+
+fun main() {
+    val backend = Backend(name = "asdf")
+    val front = Frontend(name = "dfgh")
+
+    DeveloperPool.add(backend)
+    DeveloperPool.add(front)
+
+    println(DeveloperPool.get("asdf"))
+    println(DeveloperPool.get("dfgh"))
+}
