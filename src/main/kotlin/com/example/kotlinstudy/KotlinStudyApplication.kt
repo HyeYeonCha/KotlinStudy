38a6bbd99aabaf1197cba7ebed6a986de101a793
HyeYeonCha <gpdus335@naver.com>
2022-08-29 21:03:54 +0900

    공변성, 반공변성

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,13 +1,23 @@
 package com.example.kotlinstudy
 
-class MyGenerics<T>(val t:T) {}
+// 공변성
+class MyGenerics<out T>(val t:T) {}
+
+class Bag<T> {
+    fun saveAll(
+            to: MutableList<in T>,
+            from: MutableList<T>,
+    ) {
+        to.addAll(from)
+    }
+}
 
 fun main() {
     // 제네릭을 사용한 클래스의 인스턴스를 만드려면 type argument 를 제공
 //    val generics = MyGenerics<String>("test")
 
     // type 생략 가능
-    val generics = MyGenerics("test")
+//    val generics = MyGenerics("test")
 
     // 변수의 타입에 제네릭을 사용한 경우
     val list1: MutableList<String> = mutableListOf()
@@ -16,4 +26,16 @@ fun main() {
 
     val list3: List<*> = listOf<String>("test")
     val list4: List<*> = listOf<Int>(1, 2, 3, 4)
+
+    // PECS -> Producer-Extends, Consumer-Super
+    // 공변성 -> 자바 제네릭의 extends 코틀린의 out
+    // 반공변성 -> 자바 제네릭의 super 코틀린의 in
+
+    val generics = MyGenerics<String>("test")
+    // 상위타입의 CharSequence 이므로 공변성 부여 가능
+    val charGenerics: MyGenerics<CharSequence> = generics
+
+
+    val bag = Bag<String>()
+    bag.saveAll(mutableListOf<CharSequence>("1", "2"), mutableListOf<String>("3", "4"))
 }
