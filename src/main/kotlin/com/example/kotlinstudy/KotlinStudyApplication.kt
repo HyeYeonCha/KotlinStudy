2c02e726b2dce0dce16ab7c7b6b708b574565ac6
HyeYeonCha <gpdus335@naver.com>
2022-08-29 20:49:10 +0900

    제네릭 기초

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,19 @@
 package com.example.kotlinstudy
 
-fun main() {}
+class MyGenerics<T>(val t:T) {}
+
+fun main() {
+    // 제네릭을 사용한 클래스의 인스턴스를 만드려면 type argument 를 제공
+//    val generics = MyGenerics<String>("test")
+
+    // type 생략 가능
+    val generics = MyGenerics("test")
+
+    // 변수의 타입에 제네릭을 사용한 경우
+    val list1: MutableList<String> = mutableListOf()
+    // type argument 를 생성자에서 추가
+    val list2 = mutableListOf<String>()
+
+    val list3: List<*> = listOf<String>("test")
+    val list4: List<*> = listOf<Int>(1, 2, 3, 4)
+}
