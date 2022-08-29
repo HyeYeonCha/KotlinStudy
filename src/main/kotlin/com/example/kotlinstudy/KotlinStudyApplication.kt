0fe29f19f88f4b3eedc09b59ba77e9fe48490543
HyeYeonCha <gpdus335@naver.com>
2022-08-29 20:28:40 +0900

    String 객체에 간단한 확장함수 추가

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,14 @@
 package com.example.kotlinstudy
 
-fun main() {}
+fun String.first() : Char {
+    return this[0]
+}
+
+fun String.addFirst(char: Char): String {
+    return char + this.substring(0)
+}
+
+fun main() {
+    println("asdf".first())
+    println("asdf".addFirst('A'))
+}
