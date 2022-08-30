f55ee02040dea2cae130d155790e340c485ad043
HyeYeonCha <gpdus335@naver.com>
2022-08-30 20:17:14 +0900

    간단한 지연 초기화 예제 (사용시점에 코드 초기화하기)

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,19 @@
 package com.example.kotlinstudy
 
-fun main() {}
+
+class HelloBot {
+//    var greeting: String? = null
+// 불변 변수로 바꿈
+    val greeting: String by lazy { getHello() }
+    fun sayHello() = println(greeting)
+}
+
+fun getHello() = "Hi Hello"
+
+fun main() {
+    val helloBot = HelloBot()
+
+    // 실행되면서 초기화
+//    helloBot.greeting = getHello()
+    helloBot.sayHello()
+}
