784bb1013f9a3ffcc9fc9d3eed26f7b095888c09
HyeYeonCha <gpdus335@naver.com>
2022-08-30 21:00:42 +0900

    let 예시

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,12 @@
 package com.example.kotlinstudy
 
-fun main() {}
+fun main() {
+    val str: String? = "null"
+
+    val result: Int? = str?.let {
+        println(it)
+        123
+    }
+
+    println(result)
+}
