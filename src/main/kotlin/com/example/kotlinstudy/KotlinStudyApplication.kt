aee17cfa09a4691797bb5b128fe0be2b8fa9d4ee
HyeYeonCha <gpdus335@naver.com>
2022-08-30 21:06:59 +0900

    run 예시

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,12 +1,35 @@
 package com.example.kotlinstudy
 
-fun main() {
-    val str: String? = "null"
+class RunExample {
+    var a1: String? = null
+    var a2: String? = null
 
-    val result: Int? = str?.let {
-        println(it)
-        123
+    fun exam(): Boolean {
+        println("123...")
+        Thread.sleep(1000)
+        println("...456")
+
+        return true
     }
+}
+fun main() {
+//    val str: String? = "null"
+//
+//    val result: Int? = str?.let {
+//        println(it)
+//        123
+//    }
+//
+//    println(result)
 
-    println(result)
+    val run = RunExample().run {
+        // this 가 자동으로 붙음
+        a1 = "asdfa"
+        a2 = "a2"
+        exam()
+    }
+    println(run)
+
+//    val result = run.exam()
+//    println(result)
 }
