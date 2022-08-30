f2fad79acfaeb9763011a4e1dff9773005c84535
HyeYeonCha <gpdus335@naver.com>
2022-08-30 20:23:20 +0900

    lazy 의 속성 비교

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -2,9 +2,15 @@ package com.example.kotlinstudy
 
 
 class HelloBot {
-//    var greeting: String? = null
-// 불변 변수로 바꿈
-    val greeting: String by lazy { getHello() }
+    //    var greeting: String? = null
+    // 불변 변수로 바꿈
+    // SYNCHRONIZED -> 기본값, 멀티쓰레드 환경에서 안정적으로 사용하기 위해
+    // NONE -> 쓰레드 동기화 없이 사용
+    val greeting: String by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+        println("init")
+        getHello()
+    }
+
     fun sayHello() = println(greeting)
 }
 
@@ -15,5 +21,13 @@ fun main() {
 
     // 실행되면서 초기화
 //    helloBot.greeting = getHello()
-    helloBot.sayHello()
+//    helloBot.sayHello()
+
+    // 호출을 여러번 해도 초기화 코드는 한 번만 동작한다.
+    // 멀티 쓰레드 환경에서도 안정적으로 동작
+    for (i in 1 .. 5) {
+        Thread {
+            helloBot.sayHello()
+        }.start()
+    }
 }
