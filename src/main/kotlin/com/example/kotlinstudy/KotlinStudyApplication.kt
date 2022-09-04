0fc0a6f1796d1fa011db3ed49456a7ffdd5fd0ba
HyeYeonCha <gpdus335@naver.com>
2022-09-04 20:21:21 +0900

    각종 함수형 프로그래밍 예제

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,40 @@
 package com.example.kotlinstudy
 
-fun main() {}
+fun main() {
+
+    val list = mutableListOf(printHello)
+    val exFun: () -> Unit = list[0]
+    exFun()
+
+    call(printHello)
+
+    // printNo -> fun 으로 선언해 일급객체의 특성을 가지지 않기 때문에 데이터 구조 또는 인자로 넣을 수 없음. (대입 불가)
+    // fun 키워드가 아닌 변수로 만들어야 함
+//    val exList = mutableListOf(printNo)
+//    call(printNo)
+
+    val result: Int = plus(2, 3)
+    println(result)
+}
+
+val printHello: () -> Unit = { println("Hello") }
+
+val func: () -> String = {""}
+
+// 함수를 인자로 받아 실행
+fun call(block: () -> Unit) {
+    block()
+}
+
+fun printNo() {
+    println("NO")
+}
+
+val printMessage: (String) -> Unit = { message: String -> println(message) }
+// message -> 자동 String
+val printMessage2: (String) -> Unit = { message -> println(message) }
+// 변수 없이 it 으로 줄이기
+val printMessage3: (String) -> Unit = { println(it) }
+
+val plus: (Int, Int) -> Int = { a, b -> a + b}
+
