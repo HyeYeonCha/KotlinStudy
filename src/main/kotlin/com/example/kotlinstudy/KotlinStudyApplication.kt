9ac515daed9eee401eb7c11817cc64552359f67a
HyeYeonCha <gpdus335@naver.com>
2022-09-04 19:38:15 +0900

    try catch 와 runCatching

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,20 @@
 package com.example.kotlinstudy
 
-fun main() {}
+fun getStr(): Nothing = throw Exception("throw new Exception")
+
+fun main() {
+
+//    val result = try {
+//        getStr()
+//    } catch (e: Exception) {
+//        println(e.message)
+//        "exception"
+//    }
+
+    val result = runCatching { getStr() }.getOrElse {
+        println(it.message)
+        "exception"
+    }
+
+    println(result)
+}
