8ad52bb5339e77b74eaa86e4e71ea28211ab3f5f
HyeYeonCha <gpdus335@naver.com>
2022-09-19 22:00:33 +0900

    java 와 kotlin 의 예외처리

--- src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
+++ src/main/kotlin/com/example/kotlinstudy/KotlinStudyApplication.kt
@@ -1,3 +1,19 @@
 package com.example.kotlinstudy
 
-fun main() {}
+import com.example.kotlinstudy.exception.JavaThrow
+import java.io.IOException
+
+class KotlinThrow {
+    fun throwIOException() {
+        throw IOException("체크드 익셉션인 IOException 발생")
+    }
+}
+
+fun main() {
+    val javaThrow = JavaThrow()
+    javaThrow.throwIOException()
+
+    val kotlinThrow = KotlinThrow()
+    kotlinThrow.throwIOException()
+}
+
---
+++ src/main/kotlin/com/example/kotlinstudy/exception/JavaThrow.java
@@ -0,0 +1,19 @@
+package com.example.kotlinstudy.exception;
+
+import java.io.IOException;
+
+public class JavaThrow {
+
+    public void throwIOException () throws IOException {
+        throw new IOException("체크드 익셉션인 IOException 발생");
+    }
+
+    public static void main(String[] args) {
+        JavaThrow javaThrow = new JavaThrow();
+        try {
+            javaThrow.throwIOException();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
